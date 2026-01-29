c4f37e823d6cd38d9c227287d277f5fc64253b8f
maru-dev4905 <marudev4905@gmail.com>
2026-01-29 23:07:38 +0900

    10강 study - 함수의 선택적 매개변수와 나머지 매개변수2

--- ch04/index.ts
+++ ch04/index.ts
@@ -61,4 +61,34 @@ function announceSong(song: string, singer?: string){
 }
 announceSong("RE:WIND"); //OK
 announceSong("ROCKDOWN", undefined); //OK
-announceSong("겨울봄 ", "KIDDING"); //OK
+announceSong("겨울봄", "KIDDING"); //OK
+
+// 선택적 매개변수는 항상 암묵적으로 undefined가 될 수 있다.
+// 선택적 매개변수는 | undefined를 포함하는 유니언 타입 매개변수와는 다르다.
+// ?으로 표시된 선택적 매개변수가 아닌 매개변수는 값이 명시적으로 undefined일지라도 항상 제공되어야 한다.
+
+function announceSongBy(song: string, singer: string | undefined){
+  console.log("song: ", song);
+
+  if(singer){
+    console.log("singer: ", singer);
+  }
+}
+announceSongBy("RE:WIND"); // Error: 2개의 인수가 필요한데 1개를 가져왔다.
+announceSongBy("ROCKDOWN", undefined); //OK
+announceSongBy("겨울봄", "KIDDING"); //OK
+
+// 함수에서 사용되는 모든 선택적 매개변수는 마지막 매개변수여야 한다.
+function announceSinger(singer?: string, song: string){} // Erroir: 필수 매개변수는 선택적 매개변수 뒤에 올 수 없다.
+
+// 1.3 나머지 매개변수
+// ...스프레드 연산자는 함수 선언의 마지막 매개변수에 위치하고, 해당 매개변수에서 시작해 함수에 전달된 '나머지' 인수가 모두 단일 배열에 저장되어야 함을 나타낸다.
+
+function singAllTheSongs(singer: string, ...songs: string[]){
+  for(const song of songs){
+    console.log(`${song}, by ${singer}.`);
+  }
+}
+singAllTheSongs("Lilpa"); // 결론적으로, 나머지 매개변수는 인자가 아예 전달되지 않았을 때 빈 배열로 처리되기 때문에, 인자를 명시적으로 생략하는 선택적 매개변수와는 다른 방식으로 동작한다.
+singAllTheSongs("Lilpa","VIichan","Jururu","Gosegu");
+singAllTheSongs("String", 2000); // Error: 'number' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없다.
